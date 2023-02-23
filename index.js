6ce4986bbf2f83dd7cef2141ca5228d94fe6fd48
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-02-23 13:45:00 +0100

    Buid: fix conflict

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
-import express from 'express'
+import express from "express";
 
 const url = "https://whois.fdnd.nl/api/v1/squad/squat-c-2022";
-const data = await fetch(url).then((response) => response.json()).catch((error) => error);
+const data = await fetch(url)
+  .then((response) => response.json())
+  .catch((error) => error);
 
 // Maak een nieuwe express app
-const app = express()
+const app = express();
 
 // Stel in hoe we express gebruiken
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   console.log(request.query.squad);
-    response.render('index', data);
-  })
+  response.render("index", data);
+});
 
-  // dummy datatables ambition
-  const ambitions = ['Frontend engineer', 'HTML/CSS developer', 'Frontend webdesigner', 'Not yet defined'];
-  data.squad.members.forEach(function(member) {
+// dummy datatables ambition
+const ambitions = [
+  "Frontend engineer",
+  "HTML/CSS developer",
+  "Frontend webdesigner",
+  "Not yet defined",
+];
+data.squad.members.forEach(function (member) {
   if (!member.ambition) {
-  // Pak een random ambition en zet deze in de member
-  member.ambition = ambitions[Math.floor(Math.random() * ambitions.length)]
+    // Pak een random ambition en zet deze in de member
+    member.ambition = ambitions[Math.floor(Math.random() * ambitions.length)];
   }
-  })
-
-  //  dummy datatables skills
-  const skills = ['TheWizard TheEnthusiast TheInventer TheMashUp']
-  data.squad.members.forEach(function(member) {
-    if (!member.skills) {
-      member.skills = skills(Math.floor(Math.random() * skills.length))
-    }
-  })
+});
 
 // Stel het poortnummer in en start express
-app.set('port', process.env.PORT || 8000)
-app.listen(app.get('port'), function () {
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
-
+app.set("port", process.env.PORT || 8000);
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
